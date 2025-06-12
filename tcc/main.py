af7f8b099acd624113d91e0b284da37e403e4420
Alexandre Muller Junior <alexandremj@protonmail.com>
2025-06-12 09:42:26 -0300

    check if file was previously uploaded on the chain

--- tcc/main.py
+++ tcc/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 
-from router import health, posts, token, users
+from router import check, health, posts, token, users
 
 app = FastAPI()
 
+app.include_router(check.router)
 app.include_router(health.router)
 app.include_router(posts.router)
 app.include_router(token.router)
